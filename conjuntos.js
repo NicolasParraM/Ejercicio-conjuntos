6f7c2a9e653d4e6112d5007f2f014ce81807dab8
Nicolas Parra <thndrdbeta@gmail.com>
2022-10-07 18:50:56 -0500

    Ejercicio-conjuntos

---
+++ conjuntos.js
@@ -0,0 +1,8 @@
+const Familia = ["Clara Parra", "Jose Parra", "Arlinson Parra"];
+
+const miSet = new Set(Familia);
+
+miSet.add("Nicolas Parra", "Nicolas Parra");
+console.log(miSet);
+miSet.add("Javascript");
+console.log(miSet);
